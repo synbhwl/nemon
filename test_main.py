812bb1cff7279f7b901f45515a17a8f6606d31e1
synbhwl <sayanbhowalemail@gmail.com>
2025-08-21 05:37:56 -0700

    more basic tests

--- test_main.py
+++ test_main.py
@@ -1,6 +1,6 @@
 from fastapi.testclient import TestClient
 import httpx
-from main import app, http_client,parse_page,send_req_to_groq  
+from main import app, http_client,parse_page,send_req_to_groq,Scrape_res  
 import pytest
 import asyncio
 
@@ -16,4 +16,8 @@ async def test_http_client():
 	response = await http_client("https://example.com")
 	assert isinstance(response, httpx.Response)
 
-	
+@pytest.mark.asyncio
+async def test_parse_parge():
+	res = await http_client("https://example.com")
+	payload = parse_page(res,"https://example.com")
+	assert isinstance(payload, Scrape_res)
